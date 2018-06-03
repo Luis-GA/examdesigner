7000b3a1df47458cdc6c94391b6da4c4e1b96547
Luis <wizsey@gmail.com>
2018-06-03 18:46:29 +0200

    some fixes in test and essay automatic generator implemented

--- src/main/java/util/ExamGenerator.java
+++ src/main/java/util/ExamGenerator.java
@@ -5,6 +5,7 @@ import model.Exam;
 import model.ExamPart;
 import model.Question;
 
+import javax.swing.plaf.DimensionUIResource;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -26,7 +27,7 @@ public class ExamGenerator {
             ExamPart essayPart = new ExamPart();
             ArrayList<Question> questions = new ArrayList<>();
             for (int i = 0; i < essayTopics.size(); i++) {
-                questions.addAll(databaseManager.getQuestions(essayTopics.get(i), "Essay"));
+                questions.addAll(databaseManager.getQuestions(essayTopics.get(i), "ESSAY"));
             }
 
 
@@ -35,6 +36,12 @@ public class ExamGenerator {
 
         //Test part
         if (essaypercent != 100) {
+            int weigh;
+            if(essaypercent==0)
+                weigh=100;
+            else{
+            duration=duration-parts.get(0).getDuration();
+            weigh=100-parts.get(0).getWeigh();}
             ExamPart testPart = new ExamPart();
             ArrayList<Question> questions;
             do {
@@ -44,8 +51,10 @@ public class ExamGenerator {
                 }
 
                 testPart = generateTestPart(testPart, duration, questions);
-                testPart.setWeigh(100 - essaypercent);
-            }while(testPart.getDuration()>duration || testPart.getWeigh()>100-essaypercent);
+
+            }while(testPart.getDuration()>duration || testPart.getWeigh()>weigh);
+            testPart.setWeigh(weigh);
+            testPart.setDuration(duration);
             parts.add(testPart);//Final evaluation
         }
 
@@ -56,12 +65,33 @@ public class ExamGenerator {
     }
 
     private static ExamPart generateEssayPart(ExamPart essayPart, int difficulty, int duration, int essaypercent, List<Question> questions) {
-        //TODO
-        return essayPart;
+
+        questions=randomOrderList(questions);
+
+        int EssayDuration=duration*essaypercent/100;
+
+        questions=ponderation(questions,(60/duration));
+        List<Question>  selectedQuestions= new ArrayList<>();
+
+        if(difficulty==0)//set to -1
+            selectedQuestions = EssayWithoutDiff(selectedQuestions,EssayDuration,essaypercent,questions);
+        else
+            selectedQuestions = EssayWithDiff(selectedQuestions,EssayDuration,essaypercent,questions, difficulty);
+
+
+        if (getTotalTime(selectedQuestions)>=duration*essaypercent/100*0.9 && getTotalWeight(selectedQuestions)>=essaypercent*0.9){
+        essayPart.setQuestions(selectedQuestions);
+        essayPart.setTitle("ESSAY");
+
+        essayPart.setDuration(getTotalTime(selectedQuestions));
+        essayPart.setWeigh(getTotalWeight(selectedQuestions));
+        return essayPart;}
+        else
+            throw new RuntimeException("Not valid exam");
     }
 
     private static ExamPart generateTestPart(ExamPart testPart, int duration, List<Question>questions) {
-        //TODO
+
         questions=randomOrderList(questions);
         List<Question> selectedQuestions= new ArrayList<>();
         Question question;
@@ -74,6 +104,8 @@ public class ExamGenerator {
         while(condition){
             question= questions.get(counter);
             counter++;
+            if(counter>=questions.size())
+                throw new RuntimeException("Insufficient number of questions");
             pregDuration=question.getDuration();
             if(pregDuration==-1)
                 pregDuration=2;//TODO:hacer un fichero de constantes
@@ -96,4 +128,87 @@ public class ExamGenerator {
         Collections.shuffle(questions, random);
         return questions;
     }
+    private static int getTotalTime(List<Question>questions){
+        int time=0;
+        for(Question q : questions)
+            time=time+q.getDuration();
+        return time;
+    }
+    private static int getTotalWeight(List<Question>questions){
+        int weight=0;
+        for(Question q : questions)
+            weight=weight+q.getWeight();
+        return weight;
+    }
+    private static int getTotalDiff(List<Question>questions){
+        int difficulty=0;
+        for(Question q : questions)
+            difficulty=difficulty+q.getDifficulty();
+        return difficulty;
+    }
+    private static int getAverageDiff(List<Question>questions){
+        int n=(getTotalDiff(questions)/getTotalWeight(questions));
+        return n;
+    }
+    private static List<Question> ponderation(List<Question>questions, double ponderation){
+
+        for(Question q : questions)
+            q.setWeight((int)(q.getWeight()*ponderation));
+
+        return questions;
+    }
+
+    private  static List<Question> EssayWithoutDiff(List<Question>selectedQuestions, int duration,int weigth ,List<Question>questions){
+        List<Question> quest= new ArrayList<Question>(questions);
+        if (getTotalTime(selectedQuestions)>=duration*0.9 && getTotalWeight(selectedQuestions)>=weigth*0.9)
+            return selectedQuestions;
+        else{
+            for (Question q : quest) {
+                selectedQuestions.add(q);
+                if (getTotalTime(selectedQuestions)<=duration && getTotalWeight(selectedQuestions)<=weigth)
+                ;
+                else
+                    selectedQuestions.remove(q);
+                questions.remove(q);
+                EssayWithoutDiff(selectedQuestions,duration,weigth,questions);
+                if (getTotalTime(selectedQuestions)>=duration*0.9 && getTotalWeight(selectedQuestions)>=weigth*0.9)
+                    return selectedQuestions;
+                questions.add(q);
+
+            }
+
+        }
+
+        return selectedQuestions;
+    }
+
+    private  static List<Question> EssayWithDiff(List<Question>selectedQuestions, int duration,int weigth ,List<Question>questions, int difficulty){
+        List<Question> quest= new ArrayList<Question>(questions);
+        if (getTotalTime(selectedQuestions)>=duration*0.9 && getTotalWeight(selectedQuestions)>=weigth*0.9)
+            if(getTotalDiff(selectedQuestions)==difficulty)
+                return selectedQuestions;
+        else{
+            for (Question q : quest) {
+                selectedQuestions.add(q);
+                if (getTotalTime(selectedQuestions)<=duration && getTotalWeight(selectedQuestions)<=weigth)
+                    ;
+                else
+                    selectedQuestions.remove(q);
+                questions.remove(q);
+                EssayWithoutDiff(selectedQuestions,duration,weigth,questions);
+                if (getTotalTime(selectedQuestions)>=duration*0.9 && getTotalWeight(selectedQuestions)>=weigth*0.9)
+                    if(getTotalDiff(selectedQuestions)==difficulty)
+                        return selectedQuestions;
+                questions.add(q);
+
+            }
+
+        }
+
+        return selectedQuestions;
+    }
+
+
+
 }
+
